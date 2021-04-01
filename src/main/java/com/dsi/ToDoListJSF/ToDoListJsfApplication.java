8ff1d8782e01436d9c06132fa008f5d43b242937
lubna40 <lubu7035@gmail.com>
2021-04-01 14:30:54 +0600

    Create spring boot project and paste files

---
+++ src/main/java/com/dsi/ToDoListJSF/ToDoListJsfApplication.java
@@ -0,0 +1,13 @@
+package com.dsi.ToDoListJSF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListJsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListJsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsi/ToDoListJSF/ToDoListJsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsi.ToDoListJSF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListJsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
