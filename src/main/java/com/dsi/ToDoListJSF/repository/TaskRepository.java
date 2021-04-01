491568ce2a97b611f554d9baf6280f7481fe9e11
lubna40 <lubu7035@gmail.com>
2021-04-01 15:48:20 +0600

    Update dependency, seed data, application.properties

---
+++ src/main/java/com/dsi/ToDoListJSF/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.dsi.ToDoListJSF.repository;
+
+import com.dsi.ToDoListJSF.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+}
---
+++ src/main/webapp/WEB-INF/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO task (id, title, reminder, date_time) VALUES (1000, 'Reading', true, '');
+INSERT INTO task (id, title, reminder, date_time) VALUES (1001, 'Reading', false, '');
+INSERT INTO task (id, title, reminder, date_time) VALUES (1002, 'Reading', false, '');
+INSERT INTO task (id, title, reminder, date_time) VALUES (1003, 'Reading', true, '');
+INSERT INTO task (id, title, reminder, date_time) VALUES (1004, 'Reading', false, '');
+INSERT INTO task (id, title, reminder, date_time) VALUES (1005, 'Reading', true, '');
