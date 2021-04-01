04c835e9b3e7375c4f678862544ad1cdf402bd8d
lubna40 <lubu7035@gmail.com>
2021-04-01 16:13:43 +0600

    Add TaskNewController

---
+++ src/main/java/com/dsi/ToDoListJSF/controller/TaskNewController.java
@@ -0,0 +1,30 @@
+package com.dsi.ToDoListJSF.controller;
+
+import com.dsi.ToDoListJSF.entity.Task;
+import com.dsi.ToDoListJSF.repository.TaskRepository;
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.el.ELBeanName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Scope(value = "session")
+@Component(value = "taskNewController")
+@ELBeanName(value = "taskNewController")
+@Join(path = "/", to="/")
+public class TaskNewController {
+    @Autowired
+    private TaskRepository taskRepository;
+    private Task task = new Task();
+
+    public String saveTask(){
+        taskRepository.save(task);
+        task = new Task();
+
+        return "/";
+    }
+
+    public Task getTask(){
+        return task;
+    }
+}
