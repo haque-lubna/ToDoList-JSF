702eb1c6c6ceb6edcecff28ee389134177eae41b
lubna40 <lubu7035@gmail.com>
2021-04-01 16:25:08 +0600

    Add TaskListController

---
+++ src/main/java/com/dsi/ToDoListJSF/controller/TaskListController.java
@@ -0,0 +1,35 @@
+package com.dsi.ToDoListJSF.controller;
+
+import com.dsi.ToDoListJSF.entity.Task;
+import com.dsi.ToDoListJSF.repository.TaskRepository;
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.annotation.RequestAction;
+import org.ocpsoft.rewrite.el.ELBeanName;
+import org.ocpsoft.rewrite.faces.annotation.Deferred;
+import org.ocpsoft.rewrite.faces.annotation.IgnorePostback;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Scope(value = "session")
+@Component(value = "taskList")
+@ELBeanName(value = "taskList")
+@Join(path = "/", to = "/")
+public class TaskListController {
+    @Autowired
+    private TaskRepository taskRepository;
+    private List<Task> taskList;
+
+    @Deferred
+    @RequestAction
+    @IgnorePostback
+    public void loadData(){
+        taskList = taskRepository.findAll();
+    }
+
+    public List<Task> getTaskList(){
+        return taskList;
+    }
+}
