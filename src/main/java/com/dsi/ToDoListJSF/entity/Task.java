35c307389d4c30732f74010358ba6d93890bf7c9
lubna40 <lubu7035@gmail.com>
2021-04-05 16:29:03 +0600

    Update entity properties

--- src/main/java/com/dsi/ToDoListJSF/entity/Task.java
+++ src/main/java/com/dsi/ToDoListJSF/entity/Task.java
@@ -11,18 +11,9 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    @Column
     private String name;
-
-    @Column
     private BigDecimal price;
-
-    public Task() {
-    }
-
-    public Task(String name, BigDecimal price) {
-        this.name = name;
-        this.price = price;
-    }
+    private String title;
+    private String timeDate;
+    private Boolean reminder;
 }
