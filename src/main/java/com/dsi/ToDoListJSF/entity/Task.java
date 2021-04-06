43093f7b440aeb88e50a6f766093f01f62400611
lubna40 <lubu7035@gmail.com>
2021-04-06 17:31:25 +0600

    Generate setter getter and remove Data annotation

--- src/main/java/com/dsi/ToDoListJSF/entity/Task.java
+++ src/main/java/com/dsi/ToDoListJSF/entity/Task.java
@@ -1,12 +1,9 @@
 package com.dsi.ToDoListJSF.entity;
 
-import lombok.Data;
-
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Data
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -14,4 +11,46 @@ public class Task {
     private String title;
     private Date timeDate;
     private Boolean reminder;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getTimeDate() {
+        return timeDate;
+    }
+
+    public void setTimeDate(Date timeDate) {
+        this.timeDate = timeDate;
+    }
+
+    public Boolean getReminder() {
+        return reminder;
+    }
+
+    public void setReminder(Boolean reminder) {
+        this.reminder = reminder;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", timeDate=" + timeDate +
+                ", reminder=" + reminder +
+                '}';
+    }
 }
